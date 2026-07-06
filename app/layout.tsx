c32a0bf860f58aaca27bb7c70dcd1815e399b14e
Nemanja Gajic <freelancenemanjagajic@gmail.com>
2026-07-06 18:51:42 +0200

    Add Google Analytics 4 (gtag) via next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { site } from "@/lib/site";
 import "./globals.css";
 import Loader from "@/components/Loader";
@@ -73,6 +74,18 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V9RXXZGZSZ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V9RXXZGZSZ');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(businessSchema) }}
